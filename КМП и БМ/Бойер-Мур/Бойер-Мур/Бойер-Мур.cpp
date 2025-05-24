1a7469b50194723aa6cb8fcd790d8d78941abdc6
Ксения Косиненко <kseniyakosinenko@gmail.com>
2025-05-24 23:55:04 +0500

    Add files via upload

---
+++ КМП и БМ/Бойер-Мур/Бойер-Мур/Бойер-Мур.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream> 
+#include <string> 
+#include <clocale> 
+using namespace std;
+
+void buildBadCharTable(const string& pattern, int badCharTable[256]) // Функция buildBadCharTable (построение таблицы плохих символов)
+{
+    int m = pattern.size(); // Получаем длину образа
+    for (int i = 0; i < 256; ++i) // Инициализация таблицы значением m
+    {
+        badCharTable[i] = m; // Присваиваем каждому элементу таблицы значение длины образа
+    }
+    for (int i = 0; i < m - 1; ++i) // Заполнение таблицы на основе pattern
+    {
+        badCharTable[(unsigned char)pattern[i]] = m - 1 - i; // Записываем смещение для символа в таблице
+    }
+    return;
+}
+
+int boyerMooreSearch(const string& text, const string& pattern) // Функция поиска алгоритмом Бойера-Мура
+{
+    int n = text.size(); // Получаем длину текста
+    int m = pattern.size(); // Получаем длину образа
+    if (m == 0) return -1;  // Проверки на пустые строки и длину - если образ пуст, возвращаем -1
+    if (n == 0) return -1;  // Проверки на пустые строки и длину - если текст пуст, возвращаем -1
+    if (m > n) return -1;  // Проверки на пустые строки и длину - если образец длиннее текста, возвращаем -1
+    int badCharTable[256]; // Таблица плохих символов
+    buildBadCharTable(pattern, badCharTable); // Построение таблицы
+    int shift = 0; // Смещение
+    while (shift <= n - m) // Цикл пока смещение не превышает n-m
+    {
+        for (int k = 0; k < shift; ++k) // пошаговый вывод
+        {
+            cout << " ";
+        }
+        cout << pattern << endl;
+        int j = m - 1; // Индекс для pattern (начинаем с конца)
+        while (j >= 0 && pattern[j] == text[shift + j]) // Пока j >= 0 и символы совпадают
+        {
+            j--; // Двигаемся по pattern слева направо
+        }
+        if (j < 0) // Если образ найден
+        {
+            cout << "Образец найден на позиции: " << shift << endl;
+            return shift;
+        }
+        else // Если образ не найден
+        {
+            shift += badCharTable[(unsigned char)text[shift + m - 1]]; // Сдвигаем образ
+        }
+    }
+    cout << "Образец не найден";
+    return -1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+    string text, pattern;
+
+    cout << "Введите текст: " << endl;
+    getline(cin, text);
+
+    cout << "Введите образец: ";
+    getline(cin, pattern);
+
+    boyerMooreSearch(text, pattern);
+
+    return 0;
+}
---
+++ КМП и БМ/Кнут-Моррис/Кнут-Моррис/Кнут-Моррис.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+using namespace std;
+
+// Вычисляет LPS-массив для KMP
+void computeLPSArray(string pattern, int M, int* lps)
+{
+    int len = 0; // Длина самого длинного префикса, который также является суффиксом
+    lps[0] = 0; // Первый элемент LPS-массива всегда 0
+    int i = 1; // Индекс для прохода по строке pattern
+    while (i < M) // Цикл пока индекс i меньше длины строки pattern (M)
+    {
+        if (pattern[i] == pattern[len]) // Если i-ый символ в pattern равен len-ому символу 
+        {
+            len++; // Увеличиваем длину 
+            lps[i] = len; // Записываем новую длину 
+            i++; // Увеличиваем индекс
+        }
+        else //если символы не совпадают
+        {
+            if (len != 0) // Если len не равно 0 (уже есть префикс)
+            {
+                len = lps[len - 1]; // "Откат" к предыдущему более короткому префиксу
+            }
+            else // если нет префикса
+            {
+                lps[i] = 0; //присваиваем 0
+                i++; // увеличиваем индекс
+            }
+        }
+    }
+}
+
+// Поиск подстроки с использованием алгоритма Кнута-Морриса-Пратта (KMP)
+void KMPSearch(string pattern, string text)
+{
+    int M = pattern.length(); // длина строки 
+    int N = text.length(); // длина образа
+    int* lps = new int[M]; //дин память для лпс массива
+    computeLPSArray(pattern, M, lps); // вычисляем лпс массив
+    int i = 0;  // Индекс для text[]
+    int j = 0;  // Индекс для pattern[]
+    bool f = false; // флаг показывающий найден образец хотя бы раз 
+    while (i < N) // Цикл пока индекс i меньше длины строки text (N)
+    {
+        for (int k = 0; k < i - j; k++) // визуализация как двигается образец
+        {
+            cout << " ";
+        }
+        cout << pattern << endl;
+
+        if (pattern[j] == text[i])  // если символы совпадабт 
+        {
+            j++; // увеличиваем индексы
+            i++;
+        }
+        else if (i < N && pattern[j] != text[i]) // Если символы не совпадают и i < N
+        {
+            if (j != 0) // Если j не равно 0 (было частичное совпадение)
+            {
+                j = lps[j - 1]; // Используем LPS-массив для сдвига образца
+            }
+            else // нет совпадений 
+            {
+                i++; // увеличиваем индекс 
+            }
+        }
+
+        if (j == M) // если образец найден 
+        {
+            cout << "Образец найден на позиции: " << i - j << endl;
+            j = lps[j - 1];
+            f = true;
+        }
+    }
+    if (!f)  // если образец не найден 
+    {
+        cout << "Образ не найден";
+    }
+    delete[] lps;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+    string text, pattern;
+
+    cout << "Введите текст: " << endl;
+    getline(cin, text);
+
+    cout << "Введите образ: ";
+    getline(cin, pattern);
+
+    KMPSearch(pattern, text);
+
+    return 0;
+}
